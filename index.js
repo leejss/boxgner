3dd5cb86b9548a7c4e62c1f59a2e263ce39b926d
leejss <goldemshine@gmail.com>
2022-09-14 13:58:21 +0900

    Step 2

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
 const fs = require("fs");
-// const cli = require("cac")();
+const cli = require("cac")();
+const { getFileExtension, getFileName } = require("./lib/utils");
 
-// cli.option("--name <name>", "Choose a dir name");
-// cli.help();
+const REGISTERED_FLAGS = ["f", "file", "d", "dir", "h", "help", "s", "snippet"];
 
-// const parsed = cli.parse();
-// console.log(parsed.options);
+cli.option("-f", "--file <filename>", "Type a file name");
+cli.option("-d", "--dir <dirname>", "Type a dir name");
+cli.option("-s", "--snippet <snippet>", "Type a snippet name");
+cli.help();
+const parsed = cli.parse();
 
-// if ("name" in parsed.options) {
-//   fs.mkdirSync(parsed.options.name);
-//   fs.writeFileSync(
-//     `${parsed.options.name}/index.tsx`,
-//     fs.readFileSync("template/index.tsx")
-//   );
-// } else {
-//   throw new Error("Invalid flag");
-// }
+handleUnregisteredOption(parsed.options);
+handleParsedOption(parsed.options);
 
-// console.log(JSON.stringify(parsed, null, 2));
-
-// npx boxgner lib
-// npx boxgner -t page
-
-// https://ourcodeworld.com/articles/read/393/how-to-create-a-global-module-for-node-js-properly
-
-const file = fs.readFileSync("template.json");
-const fileObj = JSON.parse(file);
-
-for (const line of fileObj.tsx) {
-  const replaced = line.replace("$FILENAME", "Button");
-  fs.writeFileSync("Button.tsx", `${replaced}\n`, {
-    flag: "a",
-  });
+function handleUnregisteredOption(options) {
+  for (const flag of REGISTERED_FLAGS) {
+    if (flag in options) return;
+  }
+  throw new Error("Unregistered options");
 }
 
-// boxgner Button.tsx
+function handleFileOption(filename, snippetName) {
+  if (typeof filename === "boolean") throw new Error("Type a filename");
+  const snippetObj = JSON.parse(fs.readFileSync("snippet.json"));
 
-// use string replace
+  let snippet = [];
+  if (snippetName) {
+    snippet = snippetObj[snippetName];
+  } else {
+    const extension = getFileExtension(filename);
+    snippet = snippetObj[extension];
+  }
+
+  const name = getFileName(filename);
+  for (const line of snippet) {
+    const replaced = line.replace("$FILENAME", name);
+    fs.writeFileSync(filename, `${replaced}\n`, {
+      flag: "a",
+    });
+  }
+}
+function handleDirOption(dirname) {
+  if (typeof dirname === "boolean") throw new Error("Type a dirname");
+}
+
+function handleParsedOption(options) {
+  const snippet = options.snippet || options.s;
+
+  if ("f" in options) handleFileOption(options.f, snippet);
+  if ("file" in options) handleFileOption(options.file, snippet);
+  if ("d" in options) handleDirOption(options.d);
+  if ("dir" in options) handleDirOption(options.dir);
+}
+
+// https://ourcodeworld.com/articles/read/393/how-to-create-a-global-module-for-node-js-properly
